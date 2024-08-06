10af4e21dbdb875352d1c6e873da3825a08dfdb8
Ivan Mondok <ivanmondok8@gmail.com>
2024-08-06 19:46:39 +0300

    make build

---
+++ index.0ad0bec5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,r=!0,l=!1,t=void 0;try{for(var o,n=e[Symbol.iterator]();!(r=(o=n.next()).done);r=!0){var a=o.value.cells;a[a.length-1].before(a[1].cloneNode(!0))}}catch(e){l=!0,t=e}finally{try{r||null==n.return||n.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.0ad0bec5.js.map
